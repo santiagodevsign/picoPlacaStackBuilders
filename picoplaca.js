c2f1b79514c5f23291715221f7822c115bf1dc8e
santiagodevsign <83424867+santiagodevsign@users.noreply.github.com>
2021-04-30 21:52:07 -0500

    Diseno estetico responsive y validaciones

--- picoplaca.js
+++ picoplaca.js
@@ -1,17 +1,12 @@
-
+//RevisionCompleta toma los 3 inputs solicitados
 function RevisionCompleta() {   
     let placa = document.getElementById('placa').value
     let fecha = document.getElementById('fecha').value
     let hora = document.getElementById('hora').value
-    /*
-    placa = 'abc1230'
-    fecha = '01-05'
-    hora = '09-00'*/
-    //alert(placa + fecha + hora);
-    console.log(placa, fecha, hora)
-    //Validamos Inputs
-    Validar(placa, fecha, hora);
 
+    //Validamos Inputs
+    ValidarInputFecha(fecha, hora);
+    ValidarInputPlaca(placa)
     //Colocamos los valores del input en formato fecha/hora
     let fechaCompleta = FormatoHora(fecha, hora);
 
@@ -20,20 +15,20 @@ function RevisionCompleta() {
 
     //Falso -> Dia de restriccion por placa
     let boolDiaPlaca = VerificaDiaYPlaca(fechaCompleta, placa);
-    console.log('diaplaca'+boolDiaPlaca + 'hora'+boolHora)
+
     //Comparacion restricciones
    ComparoRestricciones(boolDiaPlaca,boolHora);
 }
+
+//RevisionHoy toma la fecha y hora del instante que se aplasta el boton
 function RevisionHoy(){
     let hoy = new Date();
-    console.log(hoy);
     let placa = document.getElementById('placa').value
+    ValidarInputPlaca(placa)
     //Falso -> hora de restriccion
     let boolHora = VerificaHora(hoy);
-
     //Falso -> Dia de restriccion por placa
     let boolDiaPlaca = VerificaDiaYPlaca(hoy, placa);
-    console.log('diaplaca'+boolDiaPlaca + 'hora'+boolHora)
     //Comparacion restricciones
     ComparoRestricciones(boolDiaPlaca,boolHora);
    
@@ -46,43 +41,38 @@ function ComparoRestricciones(boolDiaPlaca,boolHora){
     else{
         if(boolDiaPlaca == true)(alert('Gracias por usar Predictor\n\nPuede Circular Todo el día'))
         if(boolDiaPlaca == false && boolHora == true)alert('Gracias por usar Predictor\n\nSu auto tiene restricción, pero puede circular en este momento\nTenga en cuenta los horarios de restricción');
-        
     }
 }
 
-function Validar(p,f,h){
-    if(p.length !== 7 ) alert('Placa Incorrecta');
-    if(f.length !==5) alert('Fecha Incorrecta');
-    if(h.length !==5) alert('Hora Incorrecta');
+function ValidarInputPlaca(p){
+    if(p.length !== 7 ) alert('Verifique el valor ingresado\nLa Placa debe constar de 3 letras y 4 números');
+    if(isNaN(p.substring(3,7)))alert('Verifique el valor ingresado\nEn los 4 dígitos de la placa se detectó un error de ingreso');
 }
 
+function ValidarInputFecha(f,h){
+    if(f.length !==5) alert('Fecha Incorrecta, escríbala con el formato indicado, por favor');
+    if(h.substring(0,2)>24 || h.substring(3,5)>59) alert('Hora Incorrecta, escríbala con el formato indicado, por favor')
+    if(f.substring(0,2)>31 || f.substring(3,5)>12) alert('Fecha Incorrecta, escríbala con el formato indicado, por favor')
+    if(h.length !==5) alert('Hora Incorrecta, escríbala con el formato indicado, por favor');
+}
 function FormatoHora(f,h){
     let hora = h.substring(0,2);
     let minutos = h.substring(3,5);
-    console.log(hora + ' ' + minutos);
     let mes = f.substring(3,5);
     let dia = f.substring(0,2);
-    console.log(dia + ' ' + mes);
     let anio = new Date().getFullYear();
     let fecha = new Date(anio, (mes-1), dia, hora, minutos);
-    console.log(fecha);
-  
     return fecha    
 }
 
 function VerificaDiaYPlaca(fechaCompleta, placa) {
-    
-    console.log(fechaCompleta);
     let ultimoDigito = placa.substring(6,7)
-    console.log('ultimodig'+ultimoDigito)
     let diaSemana = fechaCompleta.getDay();
-    console.log('diasemana'+diaSemana);
     let resultado //Bool
     if(diaSemana == 0 || diaSemana == 6){
         resultado=true;
     } 
     else {
-        console.log(ultimoDigito);
         if (ultimoDigito == 1 || ultimoDigito == 2)
         (diaSemana == 1)? resultado= false: resultado=true;
         if (ultimoDigito == 3 || ultimoDigito == 4)
@@ -94,14 +84,10 @@ function VerificaDiaYPlaca(fechaCompleta, placa) {
         if (ultimoDigito == 9 || ultimoDigito == 0)
         (diaSemana == 5)? resultado= false: resultado=true;
     }
-
-    console.log('diaplaca'+resultado);
     return resultado;
 }
 
-function VerificaHora(fecha){
-    console.log(fecha.getHours() + ':' + fecha.getMinutes());
-    
+function VerificaHora(fecha){    
     let resultado = true
     if(fecha.getHours()>=7 && fecha.getHours()<=9){
         (fecha.getHours()==9 && fecha.getMinutes()>=31) ? resultado = true:resultado =  false;
@@ -109,11 +95,8 @@ function VerificaHora(fecha){
     else{
         if(fecha.getHours()>=16 && fecha.getHours()<=19){
             (fecha.getHours()==19 && fecha.getMinutes()>=31)? resultado = true: resultado =  false;
-            
-        }
-        
+        }     
     }
-    console.log('hora'+' '+resultado)
     return resultado;
 }
 
